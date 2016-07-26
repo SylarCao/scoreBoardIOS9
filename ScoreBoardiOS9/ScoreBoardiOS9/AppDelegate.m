546caef369a8b8b450854decdc3db7baa79d0b97
sylar <sylar@Sylar-Mac-Mini-2.local>
2016-07-26 15:40:08 +0800

    启动的save放到线程里，开机图片

--- ScoreBoardiOS9/ScoreBoardiOS9/AppDelegate.m
+++ ScoreBoardiOS9/ScoreBoardiOS9/AppDelegate.m
@@ -40,8 +40,12 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[SBLocalSaveHelper share] autoSaveOnEnterBackground];
+    });
     
-    [[SBLocalSaveHelper share] autoSaveOnEnterBackground];
+    
+    
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
